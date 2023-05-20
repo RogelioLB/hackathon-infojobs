691e0d153994c420cd8bdd4319d0cbc677224c23
RogelioLB <rogelio20052011@gmail.com>
2023-05-19 20:21:17 -0700

    Production

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,11 @@
 "use client"
-import { useRouter, useSearchParams } from "next/navigation"
+import { useSearchParams } from "next/navigation"
 import ListOffers from "../../../components/ListOffers/ListOffers"
 import ListSkills from "../../../components/ListSkills/ListSkills"
 import Loader from "../../../components/Loader/Loader"
 import SearchBar from "../../../components/SearchBar/SearchBar"
 import useCurriculum from "../../../hooks/useCurriculum"
 import useToken from "../../../hooks/useToken"
-import { CurriculumSkills } from "../../../types"
 
 export default function Home(){
     const searchParams = useSearchParams()
